8f9765d5fa5644eee7f7d1b5c13d8135e53bd5e2
Ori Gilad <ori.gilad96@gmail.com>
2016-10-28 00:35:55 -0700

    added star rating, nbd

--- app/controllers/logged-adventure.js
+++ app/controllers/logged-adventure.js
@@ -9,7 +9,17 @@ othersVisited: "525000 others visited",
 
 isEditing: false,
 
+isNotEditing: Ember.computed('isEditing', function(){
+  return !this.get('isEditing');
+}),
+
+starRating: 0,
+
 actions: {
+    logStars(rating) {
+      this.set('starRating', rating);
+    },
+
     toggleEdit() {
       this.set('isEditing', !this.get('isEditing'));
     }
